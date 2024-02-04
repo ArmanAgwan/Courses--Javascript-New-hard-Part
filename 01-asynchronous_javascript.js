378d5297aeae2f624af29901444e45edf688191b
ArmanAgwan <armanagwa@gmail.com>
2024-02-04 12:46:02 +0530

    code

---
+++ 01-asynchronous_javascript.js
@@ -0,0 +1,29 @@
+// const num = 3;
+// function multiplyBy2 (inputNumber){
+//  const result = inputNumber*2;
+//  return result;
+// }
+// const output = multiplyBy2(4);
+// const newOutput = multiplyBy2(10);
+
+
+//      Asynchronous web browser     //
+
+// function printHello(){
+//     console.log("Hello");
+//    }
+//    setTimeout(printHello,1000);
+//    console.log("Me first!");
+
+
+//     calling the outside world    //
+
+function printHello(){
+    console.log("Hello");
+   }
+   function blockFor1Sec(){
+    //blocks in the JavaScript thread for 1 second
+   }
+   setTimeout(printHello,0);
+   blockFor1Sec()
+   console.log("Me first!");
