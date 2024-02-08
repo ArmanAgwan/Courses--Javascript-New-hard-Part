5df49ea146bc412ddf48f77f9c5570fbe2582641
ArmanAgwan <armanagwa@gmail.com>
2024-02-08 10:14:17 +0530

    code

---
+++ 05-final.js
@@ -0,0 +1,7 @@
+async function createFlow(){
+    console.log("Me first")
+    const data = await fetch('https://twitter.com/will/tweets/1')
+    console.log(data)
+   }
+   createFlow()
+   console.log("Me second")
